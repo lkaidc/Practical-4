2841893fb365dab0b8401669fd0fdd40b4c9b476
drooful <alerdrewcurtis@gmail.com>
2026-07-14 14:47:34 +0800

    Updated CSS, HTML, JS

---
+++ JS/place-order.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const placeOrderButton =
+        document.querySelector(".place-order-btn");
+
+    placeOrderButton.addEventListener("click", function () {
+        alert(
+            "This button is for demonstration purposes only."
+        );
+    });
+
+});
